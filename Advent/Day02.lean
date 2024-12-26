b8e5a8f0bc418701eb6585e973cd8bc036518bcb
Palani Johnson <palanijohnson@gmail.com>
2024-12-26 04:41:48 +0000

    solved part 1

--- Advent/Day02.lean
+++ Advent/Day02.lean
@@ -3,14 +3,35 @@ import Advent.Utils
 
 open Std.Internal.Parsec.String
 
-def ProblemInput := List (List Int)
+def Reports := List (List Int)
 
-def inputParser : Parser ProblemInput := sepBy (sepBy (digits) (skipChar '\n')) (skipChar '\n')
+def inputParser : Parser Reports := sepBy (sepBy (digits) (skipChar ' ')) (skipChar '\n')
 
-def solve1 (problemInput : ProblemInput) :=
-  "Not Implemented"
+inductive Run where
+  | incresing
+  | decresing
+  | notsafe
+deriving BEq
 
-def solve2 (problemInput : ProblemInput) :=
+def compareTwo (a b : Int) :=
+  let diff := (a - b).natAbs
+
+  if [1, 2, 3].contains diff then
+    if a > b then
+      Run.decresing
+    else
+      Run.incresing
+  else
+    Run.notsafe
+
+def isSafe (report : List Int) :=
+  let runs := report.zipWith compareTwo report.tail!
+
+  runs.all (· == Run.decresing) || runs.all (· == Run.incresing)
+
+def solve1 (reports : Reports) := (reports.map isSafe).count true
+
+def solve2 (reports : Reports) :=
   "Not Implemented"
 
 def main (args : List String) : IO Unit := do
