14ef0feb4073be86d1736de151426d6236ff237d
Palani Johnson <palanijohnson@gmail.com>
2024-12-27 02:34:02 +0000

    day 5 part 2

--- Advent/Day05.lean
+++ Advent/Day05.lean
@@ -19,21 +19,43 @@ def pageNumbersParser : Parser PageNumbers := sepBy (sepBy digits (skipChar ',')
 def ordered (orderingRules : OrderingRules) (fst : Int) (snd : Int) :=
     orderingRules.all ( λ (a, b) => ¬(snd == a ∧ fst == b))
 
-def solve1 (orderingRules : OrderingRules) (pageNumbers : PageNumbers) :=
-  let rec isOrdered (pageNums : List Int) := match pageNums with
-    | [] => true
-    | head :: tail => (tail.all (ordered orderingRules head ·)) ∧ (isOrdered tail)
+def isOrdered (orderingRules : OrderingRules) (pageNums : List Int) :=  match pageNums with
+  | [] => true
+  | head :: tail => (tail.all (ordered orderingRules head ·)) ∧ (isOrdered orderingRules tail)
 
+
+def solve1 (orderingRules : OrderingRules) (pageNumbers : PageNumbers) :=
   pageNumbers.map (λ pageNums =>
-    if isOrdered pageNums then
+    if isOrdered orderingRules pageNums then
       pageNums.get! (pageNums.length / 2)
     else
       0
   )
   |>.sum
 
-def solve2 (_ : OrderingRules) (_ : PageNumbers) :=
-  NotImplemented
+def solve2 (orderingRules : OrderingRules) (pageNumbers : PageNumbers) :=
+  let rec quickSort (pageNums : List Int) : List Int := match pageNums with
+    | [] => []
+    | head :: rest =>
+      let lesser := rest.filter (λ e => ordered orderingRules head e)
+      let greater := rest.filter (λ e => not <| ordered orderingRules head e)
+
+      (quickSort lesser) ++ [head] ++ (quickSort greater)
+  termination_by pageNums.length
+  decreasing_by
+    all_goals simp_wf
+    . sorry
+    . sorry
+
+
+  pageNumbers.map (λ pageNums =>
+    if not (isOrdered orderingRules pageNums) then
+      let pageNums := quickSort pageNums
+      pageNums.get! (pageNums.length / 2)
+    else
+      0
+  )
+  |>.sum
 
 def main (args : List String) : IO Unit := do
   match args with
