8bc251cd6113d536e4cf7ce87898e278cc79efba
Palani Johnson <palanijohnson@gmail.com>
2024-12-29 00:25:11 +0000

    day 8 part 2 done

--- Advent/Day08.lean
+++ Advent/Day08.lean
@@ -8,38 +8,62 @@ structure Point where
   y : Int
 deriving BEq, Hashable
 
-def getAntinode (p1 p2 : Point) : List Point :=
-  [
-    {
-      x := 2 * p1.x - p2.x,
-      y := 2 * p1.y - p2.y
-    },
-    {
-      x := 2 * p2.x - p1.x,
-      y := 2 * p2.y - p1.y
-    },
-  ]
+def getAntinode (p1 p2 : Point) : Point :=
+  { x := 2 * p1.x - p2.x, y := 2 * p1.y - p2.y }
 
-def countAntinodes
+def inBounds (size p : Point) := p.x >= 0 && p.x < size.x && p.y >= 0 && p.y < size.y
+
+def countAntinodesV1
   (antennaPoints : Std.HashMap Char (Std.HashSet Point))
   (size : Point)
-  : IO Int
-:= do
+  : Int
+:= Id.run do
   let mut antinodes : Std.HashSet Point := {}
+  let getAntinodes (p1 p2 : Point) : List Point := [
+    getAntinode p1 p2,
+    getAntinode p2 p1,
+  ].filter (inBounds size ·)
 
   for (_, points) in antennaPoints do
     for p1 in points do
       for p2 in points do
         if p1 != p2 then
-          antinodes := antinodes.insertMany (
-              getAntinode p1 p2
-              |>.filter ( λ p =>
-                p.x >= 0
-                && p.x <= size.x
-                && p.y >= 0
-                && p.y <= size.y
-              )
-            )
+          antinodes := antinodes.insertMany (getAntinodes p1 p2)
+
+  return antinodes.size
+
+
+def countAntinodesV2
+  (antennaPoints : Std.HashMap Char (Std.HashSet Point))
+  (size : Point)
+  : IO Int
+:=  do
+  let mut antinodes : Std.HashSet Point := {}
+  let getAntinodes (p1 p2 : Point) : List Point := Id.run do
+    let mut antinodes := [p1, p2]
+
+    let mut n1 := getAntinode p1 p2
+    let mut n2 := p1
+
+    while inBounds size n1 do
+      antinodes := n1 :: antinodes
+      (n1, n2) := (getAntinode n1 n2, n1)
+
+    n1 := getAntinode p2 p1
+    n2 := p2
+
+    while inBounds size n1 do
+      antinodes := n1 :: antinodes
+      (n1, n2) := (getAntinode n1 n2, n1)
+
+    antinodes
+
+
+  for (_, points) in antennaPoints do
+    for p1 in points do
+      for p2 in points do
+        if p1 != p2 then
+          antinodes := antinodes.insertMany (getAntinodes p1 p2)
 
   return antinodes.size
 
@@ -55,7 +79,7 @@ def main (args : List String) : IO Unit := do
 
     for (x, row) in fileContent.trim.splitOn "\n" |>.enum do
       for (y, antenna) in row.trim.data.enum do
-        if antenna != '.' then
+        if antenna != '.' && antenna != '#' then
           antennaPoints := antennaPoints.insert antenna (
               antennaPoints.getD antenna {}
               |>.insert {x, y}
@@ -65,10 +89,10 @@ def main (args : List String) : IO Unit := do
         sizeY := max sizeY y
       sizeX := max sizeX x
 
-    let size := {x:=sizeX, y:=sizeY}
+    let size := { x := sizeX + 1, y := sizeY + 1 }
 
     IO.println s!"Solution for {filePath} {sizeX} by {sizeY}:"
-    IO.println s!"Part 1: {<- countAntinodes antennaPoints size}"
-    IO.println s!"Part 2: {NotImplemented}"
+    IO.println s!"Part 1: {countAntinodesV1 antennaPoints size}"
+    IO.println s!"Part 2: {<- countAntinodesV2 antennaPoints size}"
 
     main rest
