c808da0e467e7c06a26535ec0f24eaded342b501
Palani Johnson <palanijohnson@gmail.com>
2025-01-06 23:22:33 +0000

    formatted flake

--- flake.nix
+++ flake.nix
@@ -5,19 +5,15 @@
     nixpkgs.url = "github:nixos/nixpkgs";
   };
 
-  outputs =
-    { nixpkgs, ... }@input:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        name = "advent-lean4-2024";
-        buildInputs = with pkgs; [
-          elan
-        ];
-      };
-
+  outputs = {nixpkgs, ...} @ input: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      name = "advent-lean4-2024";
+      buildInputs = with pkgs; [
+        elan
+      ];
     };
+  };
 }
